9903682be6206afdea81f77f0aaabc075a235ae7
Josef Striedl <josef@josefstriedl.com>
2022-02-17 10:15:22 +0100

    10:15

---
+++ aardvark.py
@@ -0,0 +1,5 @@
+import sys
+print ("Number of arguments:", len(sys.argv), "arguments")
+print ("Argument List:", str(sys.argv))
+print ("This is the name of the script: ", sys.argv[0])
+print ("arg 1 ?", sys.argv[1])
